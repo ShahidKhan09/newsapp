629ae90a99f16dfd87b87c5a3d2bdd16c3a381fe
Muhammad Shahid <contacttoshahidkhan@gmail.com>
2026-06-11 15:40:19 +0500

    Initial Commit

---
+++ newsbotapp/newsbotappApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsbotappApp.swift
+//  newsbotapp
+//
+//  Created by Shahid's MacPro on 11/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct newsbotappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
